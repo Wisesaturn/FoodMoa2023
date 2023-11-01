1c52ba608748b1ced3cd2e1c5099352b9dc8d213
Jae Han Song <rfv1479@gmail.com>
2023-11-01 12:14:54 +0900

    chore : jsconfig에 paths 추가

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
@@ -5,4 +7,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   base: '/FoodMoa2023/',
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@style': path.resolve(__dirname, './src//style'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@component': path.resolve(__dirname, './src/component'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
